958a8e9dec5bb4a75a6752867f8fc1731a71f036
sharmashivammrt <63046225+sharmashivammrt@users.noreply.github.com>
2020-05-28 23:08:08 +0530

    Game files commit

---
+++ dicindex.js
@@ -0,0 +1,32 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+ 
+var randomImage="dice"+randomNumber1+".png";
+
+var randomImgSrc="images/"+randomImage;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImgSrc);
+
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+ 
+var randomImage2="dice"+randomNumber2+".png";
+
+var randomImgSrc2="images/"+randomImage2;
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImgSrc2);
+
+
+if(randomNumber1>randomNumber2){
+document.querySelector("h1").innerHTML="Player 1 wins";
+
+
+}
+
+else if (randomNumber2>randomNumber1) {
+
+    document.querySelector("h1").innerHTML="Player 2 wins";
+}
+
+else {document.querySelector("h1").innerHTML="Draw";
+
+}
